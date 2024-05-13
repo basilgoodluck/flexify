b629ca4e07caf41b5761e1799186dcb564a5f06c
Basil Goodluck <basilgoodluck22@gmail.com>
2024-05-13 02:56:17 +0100

    Updated

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@scenes/navbar"
+
+function App() {
+
+  return (
+    <div className="app bg-gray-20">
+      Hello world
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/screens/navbar/index.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+interface Props {
+    
+}
+
+const index = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default index
